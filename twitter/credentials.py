977cfbf43ead9f85013ea65330bab35ed5e9efda
Esteban <75078886+Esteban-7@users.noreply.github.com>
2022-03-07 18:42:58 +0100

    Delete twitter directory

--- twitter/credentials.py
+++
@@ -1,6 +0,0 @@
-#bearer_token = "AAAAAAAAAAAAAAAAAAAAAFZNXAEAAAAAtMxzwTqdmLPBE2maLziI%2BMZFyqc%3DPf8IraaOUhkjkLHzaUjiPgVgKKNHswY4bVEdWW67mBB6NKR9Te"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAIRAWgEAAAAAjatIMs5ekMmgTxkWgzh2A5fq3xc%3DhNVEAe5pjUyEYRs37maD5Uui5aexMzWZIdVscEOQxB943IO4fb"
-
-headers = {"Authorization": "Bearer {}".format(bearer_token)}
-
-search_url = "https://api.twitter.com/2/tweets/search/all"
